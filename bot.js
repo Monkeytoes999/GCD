7907580a5d7f96eddecdeab2630b4b4d22bd7dd9
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2019-05-04 11:37:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1256,7 +1256,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				dbl.getVotes().then(votes => {
 				   console.log(votes)
 				});
-				console.log(dbl.getBot().votes);
+				dbl.getBot('495705429150793739').votes.then(votes => {
+					console.log(votes);
+				});
 				bot.sendMessage({
 					to: channelID,
 					message: "RUN THIS COMMAND WHILE IN A VC"
